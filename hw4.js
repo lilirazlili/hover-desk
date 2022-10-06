7a078c94c7be690c2825e74924399209d752b1e6
lilirazlili <85058312+lilirazlili@users.noreply.github.com>
2022-10-06 12:03:06 +0300

    Add files via upload

---
+++ hw4.js
@@ -0,0 +1,37 @@
+const board = document.querySelector("#board");
+const SQUARES_NUMBER = 1200;
+const colors = [
+  "#f93e58",
+  "#f30021",
+  "#d8005f",
+  "#eb3b88",
+  "#eb6aa3",
+  "#e038ad",
+  "#7d0057",
+];
+
+for (let i = 0; i < SQUARES_NUMBER; i++) {
+  const square = document.createElement("div");
+  square.classList.add("square");
+
+  square.addEventListener("mouseover", () => setColor(square));
+  square.addEventListener("mouseleave", () => removeColor(square));
+
+  board.append(square);
+}
+
+function setColor(element) {
+  const color = getRandomColor();
+  element.style.backgroundColor = color;
+  element.style.boxShadow = `0 0 2px ${color}, 0 0 10px ${color}`;
+}
+
+function removeColor(element) {
+  element.style.backgroundColor = "#1d1d1d";
+  element.style.boxShadow = `0 0 2px #000`;
+}
+
+function getRandomColor() {
+  const index = Math.floor(Math.random() * colors.length);
+  return colors[index];
+}
